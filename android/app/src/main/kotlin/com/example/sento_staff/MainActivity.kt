2333ba6c4feb73a4742b3a8caad55dceb10a3e02
Flamenate <haythem.titou@live.fr>
2023-01-21 01:13:51 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/sento_staff/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sento_staff
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:sento_staff/screens/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
