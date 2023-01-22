416319b46a4d87e4b8b70e3b1a07f673e96cf49a
Flamenate <haythem.titou@live.fr>
2023-01-22 17:06:09 +0100

    feat: create player model

---
+++ lib/models/player.dart
@@ -0,0 +1,15 @@
+class Player {
+  final int id;
+  final String name;
+  final int xp;
+  final int level;
+  final int balance;
+
+  const Player({
+    required this.id,
+    required this.name,
+    required this.xp,
+    required this.level,
+    required this.balance,
+  });
+}
