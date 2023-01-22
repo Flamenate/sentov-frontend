aa00a5240adadf3c865b1e66624bbd3bbca21f95
Flamenate <haythem.titou@live.fr>
2023-01-22 19:15:24 +0100

    feat: add game model

---
+++ lib/models/game.dart
@@ -0,0 +1,15 @@
+class Game {
+  final int id;
+  final String title;
+  final String description;
+  final int xpReward;
+  final int balanceReward;
+
+  const Game({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.xpReward,
+    required this.balanceReward,
+  });
+}
