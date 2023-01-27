aa21e93e354048e1bf7210da522e2035bc9b6349
Flamenate <haythem.titou@live.fr>
2023-01-27 23:48:07 +0100

    minor: remove unnecessary import

--- lib/screens/session_screen.dart
+++ lib/screens/session_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:sento_staff/models/session.dart';
 import 'package:sento_staff/widgets/appbar.dart';
 
