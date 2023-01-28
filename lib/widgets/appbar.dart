22a6c300b411aad9a2ae44e6d2a4d93b191bc006
Flamenate <haythem.titou@live.fr>
2023-01-28 01:07:19 +0100

    style: change appbar font & color to match main menu buttons

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -3,8 +3,14 @@ import 'package:go_router/go_router.dart';
 
 AppBar defaultAppBar(BuildContext context, {required String title}) {
   return AppBar(
-    leading: BackButton(onPressed: () => context.go("/"), color: Colors.white),
-    title: Text(title, style: TextStyle(color: Colors.white)),
+    leading: BackButton(
+        onPressed: () => context.go("/"),
+        color: Color.fromARGB(255, 253, 192, 107)),
+    title: Text(title,
+        style: TextStyle(
+            color: Color.fromARGB(255, 253, 192, 107),
+            fontFamily: "Unbounded",
+            fontSize: 20.0)),
     centerTitle: true,
     backgroundColor: Color.fromARGB(255, 90, 38, 107),
   );
